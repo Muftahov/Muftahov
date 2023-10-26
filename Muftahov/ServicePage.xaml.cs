15564fbcdd3aa8f60b91ad65e5c4795f5f26839a
Muftahov Rushan <osnova0303@gmail.com>
2023-10-26 03:09:17 +0300

    5 лаба

--- Muftahov/ServicePage.xaml.cs
+++ Muftahov/ServicePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,11 +26,67 @@ namespace Muftahov
             InitializeComponent();
             var currentServices = Muftahov_avtoserviceEntities1.GetContext().service.ToList();
             ServiceListView.ItemsSource = currentServices;
+
+
+            ComboType.SelectedIndex = 0;
+            UpdateSevices();
+        }
+        public void UpdateSevices()
+        {
+            var currentServices = Muftahov_avtoserviceEntities1.GetContext().service.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 0 && p.Discount <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 0 && p.Discount < 5)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 5 && p.Discount < 15)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 15 && p.Discount < 30)).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 30 && p.Discount < 70)).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 70 && p.Discount < 100)).ToList();
+            }
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Cost).ToList();  
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentServices = currentServices.OrderBy(p => p.Cost).ToList();
+            }
+            ServiceListView.ItemsSource = currentServices;
+        }
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateSevices();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            UpdateSevices();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSevices();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateSevices();
         }
     }
 }
